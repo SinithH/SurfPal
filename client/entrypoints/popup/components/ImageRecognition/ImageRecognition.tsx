b55248d3571df8d5edf05789c4bdd40f2432e5fa
SinithH <sinithdj@gmail.com>
2024-02-11 13:23:04 +0530

    add routing

--- client/entrypoints/popup/components/ImageRecognition/ImageRecognition.tsx
+++ client/entrypoints/popup/components/ImageRecognition/ImageRecognition.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Header from '../shared/Header';
 import ModuleNames from '../../constants/Modules';
+import Footer from '../shared/Footer';
 
 const ImageRecognition: React.FC = () => {
   return (
-    <div>
-      <Header heading={ModuleNames.IMAGE_RECOGNITION}/>
-      {/* Add content for Image Recognition screen */}
-    </div>
+    <>
+      <div>
+        <Header heading={ModuleNames.IMAGE_RECOGNITION}/>
+        {/* Add content for Image Recognition screen */}
+      </div>
+      <Footer/>
+    </>
   );
 };
 

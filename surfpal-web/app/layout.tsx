bab0605768c2b071e2ac1362bd73bd0397b3cb15
KiruluVidhumina <kirulu11@gmail.com>
2024-03-10 19:46:12 +0530

    init folder structure

---
+++ surfpal-web/app/layout.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { Poppins } from "next/font/google";
+import "./globals.css";
+import Header from "./shared/components/header";
+
+const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin']
+})
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        <Header />
+        <main className="mt-16">
+            {children}
+        </main>
+      </body>
+    </html>
+  );
+}
---
+++ surfpal-web/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import LandingPage from "./templates/landingPage";
+
+export default function Home() {
+  return (
+    <>
+      <LandingPage />
+    </>
+  );
+}

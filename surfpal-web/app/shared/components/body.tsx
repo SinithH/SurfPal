9875485086832b07c510e148d06724351b522f4b
SenuriNanayakkara2000 <senurisihara2000@gmail.com>
2024-03-11 02:30:56 +0530

    bodyPart1.1 landingPage

--- surfpal-web/app/shared/components/body.tsx
+++ surfpal-web/app/shared/components/body.tsx
@@ -62,7 +62,7 @@ const Body = () => {
                             <div className="grid gap-20">
                                     <div className="md:grid md:grid-cols-12 md:gap-6 items-center">
                                         <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 md:order-1" data-aos="fade-up">
-                                        <img src="surfpal-web/app/assets/images.png" alt="Your Image Description" className="w-full h-auto"/>
+                                        <img src="/surfpal-web/app/assets/images.png" alt="Your Image Description" className="w-full h-auto"/>
                                         </div>
                                         <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-right">
                                                 <div className="md:pr-4 lg:pr-12 xl:pr-16">

5ad8196079589bfd0f1dd6921307c6ebaab1595d
KiruluVidhumina <kirulu11@gmail.com>
2024-03-12 00:25:44 +0530

    login page ui

---
+++ surfpal-web/app/signUp/components/singUpForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Card, CardHeader, Typography } from '@material-tailwind/react'
+import Link from 'next/link'
+
+const SingUpForm = () => {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default SingUpForm
--- surfpal-web/tailwind.config.ts
+++ surfpal-web/tailwind.config.ts
@@ -5,6 +5,8 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -13,6 +15,9 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primaryPurple: "#9E00E8",
+      }
     },
   },
   plugins: [],

0fb6da08726a97434e14e5c812872195d737e8e6
KiruluVidhumina <kirulu11@gmail.com>
2024-03-12 00:43:57 +0530

    forgot password page ui

---
+++ surfpal-web/app/forgotPassword/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ForgotPasswordForm from './components/forgotPasswordForm'
+
+const forgotPasswordPage = () => {
+  return (
+    <>
+        <div className='w-full h-full flex justify-center items-center'>
+            <ForgotPasswordForm />
+        </div>
+    </>
+  )
+}
+
+export default forgotPasswordPage

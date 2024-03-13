236816fa8c2c8a1cf6df7d15da781d660bb63605
Maleesha Rodrigo <101212851+MaleeshaRodrigo@users.noreply.github.com>
2024-03-13 14:02:53 +0530

    Revert "Development"

--- client/entrypoints/popup/components/Navigation/Navigation.tsx
+++ client/entrypoints/popup/components/Navigation/Navigation.tsx
@@ -1,75 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Header from '../shared/Header';
 import ModuleNames from '../../constants/Modules';
 import Footer from '../shared/Footer';
-import browser from 'webextension-polyfill';
 
 const Navigation: React.FC = () => {
-  let contentScript = '';
-  let currentUrl = '';
-  browser.runtime.onMessage.addListener((message) => {
-    contentScript = message.textBody
-    currentUrl = message.currentUrl
-    fetchLinks();
-  });
-  const fetchLinks = async () => {
-    if (!contentScript || !currentUrl ) {
-      return;
-    }
-    const result = await fetch(import.meta.env.VITE_NAVIGATION_URL, {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        content: contentScript,
-        url: currentUrl
-      })
-    })
-    if (!result) {
-      throw new Error('failed to fetch')
-    }
-    setData(await result.json());
-  }
-  const [data, setData] = useState<INavigationResponse>();
-
   return (
     <>
-      <div>
-        <Header heading={ModuleNames.NAVIGATION} />
-        <div className="p-3">
-          <h1 className='mb-2'>The Navigation Links: </h1>
-          <hr />
-          <ul>
-            {data?.data.navigation.map((link) => {
-              return <li>{link.text}: {link.url}</li>
-            })}
-          </ul>
-          <h1 className='my-2'>The Content Links: </h1>
-          <hr />
-          <ul>
-            {data?.data.content.map((link) => {
-              return <li>{link.text}: {link.url}</li>
-            })}
-          </ul>
-        </div>
-      </div>
-      <Footer />
+    <div>
+      <Header heading={ModuleNames.NAVIGATION}/>
+      {/* Add content for Navigation */}
+    </div>
+    <Footer/>
     </>
+
   );
 };
 
 export default Navigation;
-
-interface INavigationResponse {
-  data: {
-    navigation: {
-      text: string,
-      url: string
-    }[],
-    content: {
-      text: string,
-      url: string
-    }[]
-  }
-}
--- server/navigation-assistant-service/test.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-from scrape import ascrape_playwright
-
-url = "https://www.patagonia.ca/shop/new-arrivals"
-data = asyncio.run(ascrape_playwright(url))
-print(data)
-

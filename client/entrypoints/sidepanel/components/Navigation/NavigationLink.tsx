60cf7e39fc90d2405880741232298757523daf22
Maleesha Rodrigo <maleesha.rodrigo@car-care.se>
2024-03-21 20:46:08 +0530

    SDGP-73 - Implemented the search process to Navigation

--- client/entrypoints/sidepanel/components/Navigation/NavigationLink.tsx
+++ client/entrypoints/sidepanel/components/Navigation/NavigationLink.tsx
@@ -2,7 +2,7 @@ import { Purpose } from '@/enum/purpose-enum';
 import React from 'react';
 import browser from 'webextension-polyfill';
 
-const NavigationLink: React.FC<{ textContent: string; href: string, currentUrl: string }> = ({ textContent, href, currentUrl }) => {
+const NavigationLink: React.FC<{ textContent: string; href: string}> = ({ textContent, href }) => {
     const onLinkClick = async (url: string) => {
         const [tab] = await browser.tabs.query({ active: true, lastFocusedWindow: true });
         await browser.tabs.sendMessage(tab.id!, { purpose: Purpose.NAVIGATE, url });

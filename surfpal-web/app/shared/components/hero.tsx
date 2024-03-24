69775ecef654c364aea8f3c6b9d0ba26e729c690
KiruluVidhumina <kirulu11@gmail.com>
2024-03-24 20:06:14 +0530

    new logo

--- surfpal-web/app/shared/components/hero.tsx
+++ surfpal-web/app/shared/components/hero.tsx
@@ -7,8 +7,9 @@ import HeroBg from '@/public/assets/hero-bg.svg';
 import Image from "next/image";
 import { Kanit } from "next/font/google";
 import { Typography } from "@material-tailwind/react";
-import PuzzleGIF from '@/public/assets/ezgif.com-animated-gif-maker (1).gif'
 import PuzzlePiece from '@/public/assets/puzzle-piece (1).svg'
+import SurfPalLogoGif from '@/public/assets/ezgif.com-animated-gif-maker (2).gif'
+import SurfPalLogo from '@/public/assets/varient-3.png'
 import useStore from "@/lib/store";
 
 
@@ -23,29 +24,27 @@ const Hero = () => {
     return(
         <div className={`${kanit.className} ${theme} w-full h-svh dark:text-white dark:bg-darkBg`}>
             <Image src={HeroBg} alt={""} className="fixed top-0 left-0 w-full"></Image>
-            <div className="relative md:pt-12">
-                <div className="w-full mx-auto text-center pb-6 md:pb-16 flex flex-col space-y-7">
-                <div className="text-9xl text-primaryPurple pt-5 inline-flex justify-center items-end gap-1 select-none">
-                    <span>Surfp</span>
-                    {theme === 'dark' ?
-                        <Image src={PuzzleGIF} alt={""} className="h-20 w-20 mb-4"></Image> :
-                        <Image src={PuzzlePiece} alt={""} className="h-20 w-20 mb-4"></Image>
-                    }   
-                    <span>l</span>
-                </div>
-                <Typography className="text-5xl font-bold" placeholder={undefined}>Empowering Accessibility: See Beyond Sight</Typography>
-                <Typography className="text-3xl font-bold" placeholder={undefined}>Navigate, Summerize, Customize & Engage</Typography>
-                {/* <h1 className="text-3xl font-bold text-white">
-                    <Typewriter
-                            options={{
-                            strings:[
-                                "Extension for Your Hard Time",
-                                "Just one Click"
-                            ],
-                            autoStart: true,
-                            loop: true
-                        }}/>
-                    </h1> */}
+            <div className="relative md:pt-16">
+                <div className="w-full mx-auto text-center items-center pb-6 justify-center md:pb-16 flex flex-col space-y-7">
+                    <div className=" w-1/3 flex select-none">
+                        {theme === 'dark' ?
+                            <Image src={SurfPalLogoGif} alt={""}></Image> :
+                            <Image src={SurfPalLogo} alt={""}></Image>
+                        }   
+                    </div>
+                    <Typography className="text-5xl font-bold" placeholder={undefined}>Empowering Accessibility: See Beyond Sight</Typography>
+                    <Typography className="text-3xl font-bold" placeholder={undefined}>Navigate, Summerize, Customize & Engage</Typography>
+                    {/* <h1 className="text-3xl font-bold text-white">
+                        <Typewriter
+                                options={{
+                                strings:[
+                                    "Extension for Your Hard Time",
+                                    "Just one Click"
+                                ],
+                                autoStart: true,
+                                loop: true
+                            }}/>
+                        </h1> */}
                 </div>
             </div>
             
